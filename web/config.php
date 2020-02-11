f4e5520b92068e8b831b0ba9c6589ebeae8fdf68
kepawni <a+kepawni@commodea.com>
2020-02-11 16:20:20 +0000

    Add example config for Web-based Hexastore inspector

---
+++ web/config.php
@@ -0,0 +1,9 @@
+<?php declare(strict_types=1);
+
+const REDIS_HOST = 'localhost';
+const REDIS_PORT = 6379;
+const REDIS_DB_INDEX = 0;
+const REDIS_PASSWORD = null;
+
+/** When no URL params are specified, show results for this predicate, e. g. 'instance-of' */
+const FALLBACK_PREDICATE = null;
